535296783a19b43988a7d611caee8d91cd8021b1
mahi nayak <mahi.nayak@gammaedge.io>
2026-02-04 13:54:23 +0530

    new

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,11 +1,15 @@
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 from fastapi.testclient import TestClient
 from unittest.mock import AsyncMock
 from app.main import app
 import app.main
 
-# --------------------
-# MOCK DATABASE
-# --------------------
+# ---------------- MOCK DATABASE ----------------
+
 fake_row = {
     "id": 1,
     "title": "CI Test Task",
@@ -16,34 +20,24 @@ fake_row = {
 mock_db = AsyncMock()
 mock_db.fetchrow.return_value = fake_row
 mock_db.fetch.return_value = [fake_row]
-mock_db.execute.return_value = "DELETE 1"
+mock_db.execute.return_value = "OK"
 
 # Replace real DB with mock
 app.main.db = mock_db
 
 client = TestClient(app)
 
+# ---------------- TESTS ----------------
 
-# --------------------
-# TESTS
-# --------------------
 def test_docs_available():
     response = client.get("/docs")
     assert response.status_code == 200
 
 
-def test_create_task_validation():
-    response = client.post("/tasks", json={})
-    assert response.status_code == 422
-
-
 def test_create_task_success():
     response = client.post(
         "/tasks",
-        json={
-            "title": "CI Test Task",
-            "description": "Testing CI pipeline",
-        },
+        json={"title": "CI Test Task", "description": "Testing CI pipeline"},
     )
     assert response.status_code == 200
     assert response.json()["title"] == "CI Test Task"
